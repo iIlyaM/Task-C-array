749738b4f2248e78872f220471559830ca19c85b
iIlyaM <71345635+iIlyaM@users.noreply.github.com>
2021-03-27 18:21:05 +0300

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+
+
+int main() {
+    int N;
+    cout<<"Enter size of array: "<< endl;
+    cin>>N;
+    char *arr = new char[N];
+    char *duplicateArr = new char[2*N];
+
+    for(int i = 0; i < N; ++i) {
+        cin >> arr[i];
+        duplicateArr[2 * i] = arr[i];
+        duplicateArr[2 * i + 1] = arr[i];
+    }
+    cout<<duplicateArr<<endl;
+    cout<< endl;
+
+    
+}
